763d02b81c6da2b5d5541ac67cb639bae89e801a
Kirinkun <peter.stehrer1@gmail.com>
2016-06-15 12:36:57 +0200

    Prepare for spotlight

--- main/frameExtension.js
+++ main/frameExtension.js
@@ -9,56 +9,9 @@
   /* Check this for spotlight
   http://www.tomdalling.com/blog/modern-opengl/08-even-more-lighting-directional-lights-spotlights-multiple-lights/
    */
-class ColorLightSGNode extends SGNode {
 
-  constructor(position,color, children) {
-    super(children);
-    this.position = position || [0, 0, 0];
-    this.ambient = [[1,1,1]-color,1];
-    this.diffuse = [color,1];
-    this.specular = [color,1];
-    //uniform name
-    this.uniform = 'u_light';
 
-    this._worldPosition = null;
-  }
-
-  setLightUniforms(context) {
-    const gl = context.gl;
-    //no materials in use
-    if (!context.shader || !isValidUniformLocation(gl.getUniformLocation(context.shader, this.uniform+'.ambient'))) {
-      return;
-    }
-    gl.uniform4fv(gl.getUniformLocation(context.shader, this.uniform+'.ambient'), this.ambient);
-    gl.uniform4fv(gl.getUniformLocation(context.shader, this.uniform+'.diffuse'), this.diffuse);
-    gl.uniform4fv(gl.getUniformLocation(context.shader, this.uniform+'.specular'), this.specular);
-  }
-
-  setLightPosition(context) {
-    const gl = context.gl;
-    if (!context.shader || !isValidUniformLocation(gl.getUniformLocation(context.shader, this.uniform+'Pos'))) {
-      return;
-    }
-    const position = this._worldPosition || this.position;
-    gl.uniform3f(gl.getUniformLocation(context.shader, this.uniform+'Pos'), position[0], position[1], position[2]);
-  }
-
-  computeLightPosition(context) {
-    //transform with the current model view matrix
-    const modelViewMatrix = mat4.multiply(mat4.create(), context.viewMatrix, context.sceneMatrix);
-    const original = this.position;
-    const position =  vec4.transformMat4(vec4.create(), vec4.fromValues(original[0], original[1],original[2], 1), modelViewMatrix);
-
-    this._worldPosition = position;
-  }
-
-  /**
-   * set the light uniforms without updating the last light position
-   */
-  setLight(context) {
-    this.setLightPosition(context);
-    this.setLightUniforms(context);
-  }
+class SpotLightSGNode extends LightSGNode {
 
   render(context) {
     this.computeLightPosition(context);
